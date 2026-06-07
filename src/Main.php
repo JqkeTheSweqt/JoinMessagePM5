5e75092170a69e8d4e450b8c8b8ab4b95748f241
JakeDev <168288909+JqkeTheSweqt@users.noreply.github.com>
2026-06-07 22:21:35 +0100

    Added config.yml & finished main.php
    Add config.yml to allow for customization of the join message & finished the main.php

--- src/Main.php
+++ src/Main.php
@@ -5,7 +5,19 @@ declare(strict_types=1);
 namespace Jqke207\JoinMessage;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+class Main extends PluginBase implements Listener{
 
-class Main extends PluginBase{
-
+public function onEnable(): void {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info("JoinMessage Plugin Enabled");
+    $this->saveDefaultConfig();
+}
+public function onPlayerJoin(PlayerJoinEvent $event) {
+$player = $event->getPlayer();
+$name = $player->getName();
+$messageFromConfig = $this->getConfig()->get("join-message", "Welcome to my server! [PS: Set a join message in config.yml]");
+$player->sendMessage($name . " " . $messageFromConfig,);
+}
 }
